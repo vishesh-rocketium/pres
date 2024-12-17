2ddbc25a82ca18b25d071d59415c7bbca1b635b9
vishesh-rocketium <vishesh.paliwal@rocketium.com>
2024-12-17 17:38:55 +0530

    meme app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import SoundBuzzer from "./soundBuzzer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SoundBuzzer />
+    </div>
+  );
+}
